5e83c2d698c9a58a0c39401b521591cd27fcaf97
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2023-10-03 02:26:39 +0800

    Create BTree_search.cpp

---
+++ Database Project 1/src/BTree_search.cpp
@@ -0,0 +1,24 @@
+//Search keys in the Bplus Tree 
+
+#include <iostream>
+
+#include "BplusTree.h"
+
+using namespace std; 
+
+void BTree::search (int key){
+
+    if(root==NULL){
+        cout<<"Tree is empty\n";
+    }
+
+    else
+    {
+        BtreeNode* cursor = root;
+        while(cursor->isLeaf == false){
+
+        }
+    }
+
+};
+ 
