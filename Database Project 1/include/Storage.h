093965ee548670f419c64c963bdf237af72846a1
lakshya-aga <lakshya001@e.ntu.edu.sg>
2023-10-03 01:30:35 +0800

    modified to remove contiguous Storage

--- Database Project 1/include/Storage.h
+++ Database Project 1/include/Storage.h
@@ -5,38 +5,7 @@
 using namespace std;
 
 //Defining structure for a record 
-struct RecordRow{
-    void *blockAddress;
-    short int offsetWithinBlock;
-};
-struct Record{
-    RecordRow recordAddress;
-    int gameDate;
-    short teamID;
-    short homePts;
-    short FG_PCT_home;
-    short FT_PCT_home;
-    short FG3_PCT_home;
-    short AST_home;
-    short REB_home;
-    bool HOME_TEAM_WINS;
 
-};
-
-
-
-
-
-class DataBlock{
-    
-    public:
-    int size;
-    int max_records;
-    Record *arr; 
-  
-        
-
-};
 
 class Storage{
     private :
--- Database Project 1/src/DataBlock.cpp
+++ Database Project 1/src/DataBlock.cpp
@@ -1,4 +1,4 @@
-#include "../include/Storage.h"
+#include "../include/DataBlock.h"
 #include <string>
 #include <vector>
 #include <tuple>
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
+#define NULL nullptr
 
 
 
@@ -17,12 +18,6 @@ class DataBlock{
         this->max_records = (size - sizeof(arr))/sizeof(Record*);
         this->arr =  (Record*)malloc(sizeof(Record)*max_records);
         };
-        
-      
-
-
-
-
 
         // READ Operation: Get a list of pointers to records based on one key
         vector<Record*> readRecordsByKey(short key) { // function will return a vector (a dynamic array-like container) of pointers to objects of type Record.
@@ -35,35 +30,28 @@ class DataBlock{
             return result;
     }
 
-        vector<Record*> readRecordsInRange(short key1, short key2) {
-            vector<Record*> result;
+        //INSERT
+        int insert(Record r){
 
             DataBlock* currentBlock = this; // Start from the current block
-
-            while (currentBlock != nullptr) {
-                // Iterate through records in the current block
-                for (int i = 0; i < currentBlock->getMaxRecords(); ++i) {
-                    short currentKey = currentBlock->getRecord(i).teamID;
-                    if (currentKey >= key1 && currentKey <= key2) {
-                        result.push_back(&(currentBlock->getRecord(i)));
-                    }
+            int i = 0;
+            for (i = 0; i < currentBlock->getMaxRecords(); ++i) {
+                    Record temp = currentBlock->getRecord(i);
+                    if(temp.teamID==-1)
+                    break;   
                 }
 
-                // Move to the next block (if available)
-                currentBlock = currentBlock->getNextBlock();
+            if(currentBlock->getRecord(i).teamID==-1)
+            {
+                arr[i] = r;
+                return i;
             }
-
-            return result;
+            else
+            {
+                return -1;
+            }
+            
         }
-
-        //INSERT
-        //DELETE
-
-
-
-
-
-
 
     private:
 
@@ -74,12 +62,12 @@ class DataBlock{
 
 
         // Function to get a specific record by index
-        Record& getRecord(int index) {
+        Record getRecord(int index) {
             if (index >= 0 && index < max_records) {
                 return arr[index];
             } else {
-                // return the first record in case of an error
-                return arr[0];
+                Record r;
+                r.teamID = -1;
             }
         }
 
