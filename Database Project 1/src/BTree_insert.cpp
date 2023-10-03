6de4fb113a49a25d4a8f5c33fd5d21d902f2c8e5
Varshbalaji <92350663+Varshbalaji@users.noreply.github.com>
2023-10-03 23:52:45 +0800

    Insert Start

--- Database Project 1/src/BTree_insert.cpp
+++ Database Project 1/src/BTree_insert.cpp
@@ -1,22 +1,53 @@
-//Search keys in the Bplus Tree 
-
+#include "../include/Storage.h"
+#include <string>
+#include <vector>
+#include <tuple>
 #include <iostream>
+#include <cstring>
+#include <algorithm>
+#include "../include/BplusTree.h"
 
-#include "BplusTree.h"
 
-using namespace std; 
+using namespace std;
 
-void BTree::insert(Key_Records key){
 
-    //Check if there is a root node
-    if(root=null){
+void Btree::insert(Key_Records key){
+
+    // if there is a root node
+    if(root == nullptr){
+        cout << "B+ Tree Creation In Progress.....\n" << "\n";
         root = new BTreeNode(deg);
-        root->keys[0] = key;
-        root->numKeysPerNode = 1
-        nodeCount++
+        root->keys[0] = key; //inserting 
+        root->numKeysPerNode = 1;
+        nodeCount++;
+        return;
     }
 
+
     else{
+        BTreeNode *current = root;
+        BTreeNode *parent;
+        parent = new BTreeNode(deg);
+
+        // where to store Key in leaf node 
+        while (current ->isleaf == false){
+            parent = current;
+            // the key to be inserted is larger than all keys in the internal node  
+            if (key.key_value > current->keys[current->numKeysPerNode-1].key_value){
+                current = current->child[current->numKeysPerNode]; // go to the next node
+            }
+            
+            else{
+                for(int i=0;i<current->numKeysPerNode;i++){
+                    if(key.key_value < current->keys->key_value){
+                        current = current->child[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
 
         //Call search function to check for duplicates 
 
