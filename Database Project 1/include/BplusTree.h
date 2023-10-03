33115f83a843d71cdd2bdee284b6f2cc300d7d33
Jai Jindal <jindaljai10@gmail.com>
2023-10-03 20:06:57 +0800

    Deletion

--- Database Project 1/include/BplusTree.h
+++ Database Project 1/include/BplusTree.h
@@ -7,6 +7,7 @@
 struct BTreeNode{
 
     bool isleaf;  //flag to indicate if leaf node or internal node
+    int numKeys;  // Number of keys currently in the node
     int* keys; // array of keys that stores keys associated with node 
     union {    //allows you to use either children or values based on the type of node (leaf or internal)
         BTreeNode** children; //If isLeaf is false, this array of pointers stores pointers to child nodes/internal nodes.
@@ -19,8 +20,11 @@ struct BTreeNode{
 class Btree{
 
     private:
-        int degree;
         BTreeNode* root;
+
+    public:
+        int degree;
+
         
     public:
         Btree(int degree); // constructor
@@ -32,7 +36,17 @@ class Btree{
         void remove(int key);   // Delete a key from the B+ tree
 
         void print();       // Print the B+ tree for debugging 
-};
 
+
+    private:
+        // Helper functions for deletion and balancing
+        void removeFromNode(BTreeNode* node, int keyIndex);
+        void redistributeLeafNodes(BTreeNode* leftNode, BTreeNode* rightNode);
+        void mergeLeafNodes(BTreeNode* leftNode, BTreeNode* rightNode, BTreeNode* parent, int parentKeyIndex);
+        void redistributeInternalNodes(BTreeNode* leftNode, BTreeNode* rightNode);
+        void mergeInternalNodes(BTreeNode* leftNode, BTreeNode* rightNode, BTreeNode* parent, int parentKeyIndex);
+        void handleUnderflow(BTreeNode* node, BTreeNode* parent, int keyIndex);
+        int findKeyIndex(BTreeNode* node, int key);
+    };
 
 #endif
