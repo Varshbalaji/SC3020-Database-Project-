6e6f814e43ada638edc4a61a693d2a9e752ffe96
Anushreearora <104303593+Anushreearora@users.noreply.github.com>
2023-10-03 18:23:41 +0800

    Update BTree_search.cpp

--- Database Project 1/src/BTree_search.cpp
+++ Database Project 1/src/BTree_search.cpp
@@ -6,19 +6,45 @@
 
 using namespace std; 
 
-void BTree::search (int key){
+BTreeNode* Btree::search(BTreeNode* node, int key, bool rangeflag, int key2){
 
-    if(root==NULL){
-        cout<<"Tree is empty\n";
+    //Tree is empty
+    if(node == nullptr) {
+        cout<<"B+ Tree is empty\n";
+        return nullptr;
     }
 
-    else
-    {
-        BtreeNode* cursor = root;
-        while(cursor->isLeaf == false){
+    //Traverse internal nodes 
+    if(!node->isleaf){
+        for(int i=0 ; i<node->size;++i){
+            if (key < node->keys[i]) {
+                return search(node->children[i], key, rangeflag, key2);
+            }
+            
+            //reach the end of the keys in the node 
+            if (key == node->size - 1) {
+                return search(node->children[i+1], key, rangeflag, key2);
+            }
+        }        
+    }
 
+    else { //leaf node 
+        for (int i = 0; i < node->size; ++i) {
+
+            //Key found in leaf node and not a range query
+            if (!rangeflag && node->keys[i] == key) {
+                return node; //Needs changing
+            }
+
+            //Range query
+            if (rangeflag && node->keys[i] >= key && node->keys[i] <= key2){
+                return node; //Needs changing
+            }
         }
     }
 
+    return nullptr;
 };
+
  
+
